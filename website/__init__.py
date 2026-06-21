b77365696187f8b557e462cefa613b9599898ef6
Van <vtjarcevic@gmail.com>
2026-06-21 07:37:51 +0000

    initialized a bit

---
+++ website/__init__.py
@@ -0,0 +1,13 @@
+import flask
+from flask import Flask
+from .views import views
+from .auth import auth
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'NEde R Land'
+
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
+
+    return app
