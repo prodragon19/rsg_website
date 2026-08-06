3f5b3bf5d43204fe1fa958e8c855ad631e0a4aac
Mandms <tornqvisteliaz@gmail.com>
2026-08-06 14:56:04 +0000

    l

--- website/extensions.py
+++ website/extensions.py
@@ -1,84 +1,21 @@
-# website/__init__.py
+# website/extensions.py
 
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+from flask_wtf.csrf import CSRFProtect
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
 
-from .extensions import (
-    bcrypt,
-    login_manager,
-    csrf,
-    limiter
+
+bcrypt = Bcrypt()
+
+
+login_manager = LoginManager()
+
+
+csrf = CSRFProtect()
+
+
+limiter = Limiter(
+    key_func=get_remote_address
 )
-
-db = SQLAlchemy()
-
-
-def create_app():
-
-    app = Flask(__name__)
-
-
-    app.config["SECRET_KEY"] = "CHANGE_THIS_TO_A_RANDOM_SECRET"
-
-
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
-
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-
-
-    app.config["SESSION_COOKIE_HTTPONLY"] = True
-    app.config["SESSION_COOKIE_SAMESITE"] = "Lax"
-
-
-
-    db.init_app(app)
-
-    bcrypt.init_app(app)
-
-    csrf.init_app(app)
-
-    limiter.init_app(app)
-
-
-
-    from .models import AdminUser
-
-
-    @login_manager.user_loader
-    def load_user(user_id):
-
-        return AdminUser.query.get(
-            int(user_id)
-        )
-
-
-    login_manager.init_app(app)
-
-    login_manager.login_view = "auth.login"
-
-
-
-    from .views import views
-    app.register_blueprint(
-        views
-    )
-
-
-    from .auth import auth
-    app.register_blueprint(
-        auth
-    )
-
-
-    from .admin import admin
-    app.register_blueprint(
-        admin
-    )
-
-
-
-    with app.app_context():
-
-        db.create_all()
-
-
